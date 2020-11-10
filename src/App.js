ecdf0e2d14dd63588c62a39abebc6ace19476738
Akanksha1rai <raiakanksha701@gmail.com>
2020-11-10 14:04:27 +0530

    tableau

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Tableau from './Tableau';
+
+function App() {
+  return (
+    <div className="App">
+     <Tableau />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tableau.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import tableauSoftware from 'tableau-api';
+
+const url = "http://public.tableau.com/views/RegionalSampleWorkbook/College";
+export class Tableau extends Component {
+
+    componentDidMount() {
+        this.initViz();
+    }
+    initViz = () => {
+        const options = {
+            iframe: false,
+            hideTabs: false,
+            width: '1280px',
+            height: '720px',
+        }
+        const vizContainer = this.vizContainer;
+        this.div = new window.tableauSoftware.Viz(vizContainer, url, options);
+    }
+
+     
+    render() {
+      return (
+            <div ref = {div => this.vizContainer = div } >
+                
+            </div>
+        )
+    }
+}
+
+export default Tableau
