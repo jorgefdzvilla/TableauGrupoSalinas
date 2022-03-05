d4a71eec8b886ef7dc785b5e10ff953b043eaed9
jorgefdz <fdzvillajorge@gmail.com>
2022-03-04 18:07:13 -0600

    Quitar calendarios

--- src/DemoCarousel.js
+++ src/DemoCarousel.js
@@ -7,9 +7,7 @@ import Tableau3 from "./Tableau3";
 import TableauLentoDesplazamiento from "./TableauLentoDesplazamiento";
 import TableauTVOUI from "./TableauTVOUI";
 import TableauCampanasSMS from "./TableauCampanasSMS";
-import TableauCeremonias from "./TableauCeremonias";
-import TableauCeremoniasScrum from "./TableauCeremoniasScrum";
-import TableauEventosPortafolio from "./TableauEventosPortafolio";
+
 
 
 export class  DemoCarousel extends Component {
