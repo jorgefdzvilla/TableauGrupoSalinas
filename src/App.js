25190b85ce3d58e721e36850858aef8d73d2601c
jorgefdz <fdzvillajorge@gmail.com>
2022-03-09 15:38:35 -0600

    pointerEvents: none

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{pointerEvents:"none"}}>
 
         <DemoCarousel>
         </DemoCarousel>
